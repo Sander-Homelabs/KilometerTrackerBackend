a0f6d12f156002cbac05d430be8fc5a6b9c6189c
Sandervg03 <sander.van.goch2003@gmail.com>
2026-08-22 04:21:01 +0200

    Adds start of migration

---
+++ cmd/migrate/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	_ = godotenv.Load()
+
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		log.Fatal("DATABASE_URL is not set")
+	}
+
+	migration, err := migrate.New(
+		"file://migrations",
+		databaseURL,
+	)
+	if err != nil {
+		log.Fatalf("failed to create migration instance: %v", err)
+	}
+	defer migration.Close()
+
+	if err := migration.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		log.Fatalf("migration failed: %v", err)
+	}
+
+	log.Println("migrations up to date")
+}
---
+++ migrations/1_create_schema.up.sql
@@ -0,0 +1,1 @@
+CREATE SCHEMA IF NOT EXISTS kilometer_tracker;
